8b961d31207bb8d426464fb05afede5a351cc0ea
Dmitriy Morozov <rifename@gmail.com>
2020-10-25 12:54:10 -0400

    register and login webforms commit

--- server/config/app.js
+++ server/config/app.js
@@ -5,12 +5,24 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+//modules for authentication
+let session = require('express-session');
+let passport = require('passport');
+let passportLocal = require('passport-local');
+let localStrategy = passportLocal.Strategy;
+let flash = require('connect-flash');
+
+/*
+let passportJWT = require('passport-jwt');
+let JWTStrategy = passportJWT.Strategy;
+let ExtractJWT = passportJWT.ExtractJwt;
+*/
+
 //database setup
-// database setup
 let mongoose = require('mongoose');
 let DB = require('./db');
 
-// point mongoose to the DB URI
+//point mongoose to the DB URI
 mongoose.connect(DB.URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 let mongoDB = mongoose.connection;
@@ -26,7 +38,7 @@ let booksRouter = require('../routes/book');
 
 let app = express();
 
-// view engine setup
+//view engine setup
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs'); // express -e
 
@@ -37,22 +49,47 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
 
+//setup express session
+app.use(session({
+  secret: "SomeSecret",
+  saveUninitialized: false,
+  resave: false
+}));
+
+//initialize flash
+app.use(flash());
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//passport user configuration
+
+// 1)create a User Model Instance
+let userModel = require('../models/user');
+let User = userModel.User;
+
+// 2)serialize and deserialize the User info
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/book-list', booksRouter);
 
-// catch 404 and forward to error handler
+//catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+//error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  //render the error page
   res.status(err.status || 500);
   res.render('error', { title: 'Error'});
 });
